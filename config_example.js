7500d5157b0ed94042a398e51b679bca9f88a81e
Mizore <xvz@live.com>
2021-08-07 22:13:19 +0800

    添加使用说明

--- config_example.js
+++ config_example.js
@@ -1,13 +1,13 @@
 module.exports = {
-  feed_link: 'https://api.lilithraws.cf/v1/rss/latest/file',
-  rule_list: [/.*/],
-  interval: 20 * 1000,
+  feed_link: 'https://api.lilithraws.cf/v1/rss/latest/file', // rss 链接
+  rule_list: [/.*/], // 匹配规则列表 [/Baha.*1080P/i, /Remake/]
+  interval: 60 * 1000, // 抓取间隔
   first_dl_enable: false,
   aria2_config: {
-    host: 'localhost',
-    port: 6800,
+    host: 'localhost', // Aria2 RPC 主机名
+    port: 6800, // 端口号
     secure: false,
-    secret: '',
+    secret: '', // 密钥
     path: '/jsonrpc'
   },
   aria2_dl_dir: '',
